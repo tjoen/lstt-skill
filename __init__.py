df84272b26e4967e581c2cfc007a263d05545361
Theun Kohlbeck <t.kohlbeck@gmail.com>
2018-02-17 16:52:44 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -45,6 +45,21 @@ class LsttSkill(MycroftSkill):
             require("LsttKeyword").build()
         self.register_intent(lstt_intent, self.handle_lstt_intent)
 
+    def handle_record_begin(self):
+        LOGGER.info("Begin Recording...") 
+        # If enabled, play a wave file with a short sound to audibly
+        # indicate recording has begun.
+        if config.get('confirm_listening'):
+            file = resolve_resource_file(
+                config.get('sounds').get('start_listening'))
+            if file:
+                play_wav(file)
+        #ws.emit(Message('recognizer_loop:record_begin'))
+
+    def handle_record_end(self):
+        LOGGER.info("End Recording...")
+        #ws.emit(Message('recognizer_loop:record_end'))
+
     def runpocketsphinx(self):
         self.speak("starting local speech client")
         wait_while_speaking()
@@ -60,7 +75,7 @@ class LsttSkill(MycroftSkill):
             p = pyaudio.PyAudio()
         stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
         stream.start_stream()
-        self.handle_record_begin()
+        self.handle_record_begin
       
         in_speech_bf = False
         decoder.start_utt()
@@ -84,7 +99,7 @@ class LsttSkill(MycroftSkill):
                             #selection = mychoice[reply]
                             stream.stop_stream()
                             stream.close()
-                            self.handle_record_end()
+                            self.handle_record_end
                             p.terminate()
                             pygame.quit()
                             #self.stop() 
@@ -94,20 +109,7 @@ class LsttSkill(MycroftSkill):
                 break
         decoder.end_utt()
 
-    def handle_record_begin(self):
-        LOGGER.info("Begin Recording...") 
-        # If enabled, play a wave file with a short sound to audibly
-        # indicate recording has begun.
-        if config.get('confirm_listening'):
-            file = resolve_resource_file(
-                config.get('sounds').get('start_listening'))
-            if file:
-                play_wav(file)
-        #ws.emit(Message('recognizer_loop:record_begin'))
 
-    def handle_record_end(self):
-        LOGGER.info("End Recording...")
-        #ws.emit(Message('recognizer_loop:record_end'))
 
     def stop(self):
         #cmd_output = subprocess.check_output(['sudo', 'service', 'mycroft-speech-client', 'start']) 
